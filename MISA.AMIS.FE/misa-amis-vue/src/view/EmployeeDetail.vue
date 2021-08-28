3f283bbe3f2aeb506682dbe9a6b284c187916738
Le Quy Nhat <lequynhat1999@gmail.com>
2021-08-28 17:57:25 +0700

    commit fontend form detail

---
+++ MISA.AMIS.FE/misa-amis-vue/src/view/EmployeeDetail.vue
@@ -0,0 +1,311 @@
+<template>
+  <div id="myModal" class="modal" :class="{ 'hidden-modal': isOpenModal }">
+    <div class="modal-content">
+      <div class="btn-top">
+        <div class="btn-close" title="Đóng(ESC)" @click="closeModal"></div>
+        <div class="btn-help" title="Giúp(F1)"></div>
+      </div>
+      <div class="header-modal">
+        <div class="title-modal">Thông tin nhân viên</div>
+        <div class="checkbox-customer">
+          <input
+            type="checkbox"
+            id="checkboxCustomer"
+            style="width: 16px; height: 18px"
+          />
+          <label for="checkboxCustomer" class="text-checkbox"
+            >Là khách hàng</label
+          >
+        </div>
+        <div class="checkbox-supplier">
+          <input
+            type="checkbox"
+            id="checkboxSupplier"
+            style="width: 16px; height: 18px"
+          />
+          <label for="checkboxSupplier" class="text-checkbox"
+            >Là nhà cung cấp</label
+          >
+        </div>
+      </div>
+      <div class="content-modal">
+        <div class="row width-100 flex">
+          <div class="row-wrapper width-50 flex">
+            <div class="row-left width-100 flex">
+              <div class="input pdr-8 width-35">
+                <div class="text-modal">
+                  <b>Mã <span style="color: red">*</span></b>
+                </div>
+                <input
+                  type="text"
+                  ref="txtEmployeeCode"
+                  v-model="employee.EmployeeCode"
+                  class="input-modal width-100"
+                />
+              </div>
+              <div class="input width-60">
+                <div class="text-modal">
+                  <b>Tên <span style="color: red">*</span></b>
+                </div>
+                <input type="text" class="input-modal width-100" v-model="employee.FullName"/>
+              </div>
+            </div>
+          </div>
+          <div class="row-wrapper width-50 flex">
+            <div class="row-left width-100 flex">
+              <div class="input width-35">
+                <div class="text-modal">
+                  <b>Ngày sinh</b>
+                </div>
+                <input type="date" class="input-modal width-100" v-model="employee.DateOfBirth"/>
+              </div>
+              <div class="input pdl-16 width-65">
+                <div class="text-modal">
+                  <b>Giới tính</b>
+                </div>
+                <div class="value-gender flex">
+                  <input
+                    type="radio"
+                    id="Nam"
+                    name="gender"
+                    class="radio-gender"
+                    :checked=" employee.GenderName == 'Nam' ? true : false "
+                  />
+                  <label for="Nam" class="text-radio">Nam</label>
+                  <input
+                    type="radio"
+                    id="Nữ"
+                    name="gender"
+                    class="radio-gender"
+                    :checked=" employee.GenderName == 'Nữ' ? true : false "
+                  />
+                  <label for="Nữ" class="text-radio">Nữ</label>
+                  <input
+                    type="radio"
+                    id="Khác"
+                    name="gender"
+                    class="radio-gender"
+                    :checked=" employee.GenderName == 'Không xác định' ? true : false "
+                  />
+                  <label for="Khác" class="text-radio">Khác</label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row-bellow width-100 flex">
+          <div class="row-wrapper width-50 flex">
+            <div class="row-left width-100 flex">
+              <div class="input width-95">
+                <div class="text-modal">
+                  <b>Đơn vị <span style="color: red">*</span></b>
+                </div>
+                <input type="text" class="input-modal width-100"  v-model="employee.DepartmentName"/>
+              </div>
+            </div>
+          </div>
+          <div class="row-wrapper width-50 flex">
+            <div class="row-left width-100 flex">
+              <div class="input pdr-8 width-60">
+                <div class="text-modal">
+                  <b>Số CMND </b>
+                </div>
+                <input type="text" class="input-modal width-100" v-model="employee.IdentityNumber"/>
+              </div>
+              <div class="input width-35">
+                <div class="text-modal">
+                  <b>Ngày cấp</b>
+                </div>
+                <input type="date" class="input-modal width-100" v-model="employee.IdentityDate"/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row-bellow width-100 flex">
+          <div class="row-wrapper width-50 flex">
+            <div class="row-left width-100 flex">
+              <div class="input width-95">
+                <div class="text-modal">
+                  <b>Chức danh</b>
+                </div>
+                <input type="text" class="input-modal width-100" v-model="employee.PositionName"/>
+              </div>
+            </div>
+          </div>
+          <div class="row-wrapper width-50 flex">
+            <div class="row-left width-100 flex">
+              <div class="input width-95">
+                <div class="text-modal">
+                  <b>Nơi cấp</b>
+                </div>
+                <input type="text" class="input-modal width-100" v-model="employee.IdentityPlace"/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row-bellow pdt-16 width-100 flex">
+          <div class="row-left width-100 flex">
+            <div class="input pdr-20 width-100">
+              <div class="text-modal">
+                <b>Địa chỉ</b>
+              </div>
+              <input type="text" class="input-modal width-100" v-model="employee.Address"/>
+            </div>
+          </div>
+        </div>
+        <div class="row-bellow width-100 flex">
+          <div class="input pdr-8">
+            <div class="text-modal">
+              <b>ĐT di động</b>
+            </div>
+            <input type="text" class="input-modal width-100" v-model="employee.PhoneNumber"/>
+          </div>
+          <div class="input pdr-8">
+            <div class="text-modal">
+              <b>ĐT cố định</b>
+            </div>
+            <input type="text" class="input-modal width-100" v-model="employee.HotLine"/>
+          </div>
+          <div class="input pdr-8">
+            <div class="text-modal">
+              <b>Email</b>
+            </div>
+            <input type="text" class="input-modal width-100" v-model="employee.Email"/>
+          </div>
+        </div>
+        <div class="row-bellow width-100 flex">
+          <div class="input pdr-8">
+            <div class="text-modal">
+              <b>Tài khoản ngân hàng</b>
+            </div>
+            <input type="text" class="input-modal width-100" v-model="employee.AccountNumber"/>
+          </div>
+          <div class="input pdr-8">
+            <div class="text-modal">
+              <b>Tên ngân hàng</b>
+            </div>
+            <input type="text" class="input-modal width-100" v-model="employee.BankName"/>
+          </div>
+          <div class="input pdr-8">
+            <div class="text-modal">
+              <b>Chi nhánh</b>
+            </div>
+            <input type="text" class="input-modal width-100" v-model="employee.BranchName"/>
+          </div>
+        </div>
+      </div>
+      <div class="paging-modal flex">
+        <div class="btn-cancel-modal">
+          <button class="btn-white" @click="closeModal">
+            <div class="text-add">Hủy</div>
+          </button>
+        </div>
+        <div class="btn-save">
+          <button class="btn-white">
+            <div class="text-add">Cất</div>
+          </button>
+        </div>
+        <div class="btn-save-add">
+          <button class="m-btn">
+            <div class="text-add">Cất và thêm</div>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import moment from "moment";
+export default {
+  name: "EmployeeDetail",
+  props: ["isOpenModal"],
+  data() {
+    return {
+      // object nhân viên
+      employee: {},
+      employeeId: "",
+      // mode cho modal : 0 là add - 1 là edit
+      mode: 0,
+    };
+  },
+  methods: {
+    /**-------------------------------------------
+     * Hàm đóng modal
+     * CreatedBy: LQNHAT(28/08/2021)
+     */
+    closeModal() {
+      this.$emit("openModal");
+    },
+
+    /**--------------------------------------------
+     * Hàm check mode
+     * CreateBy : LQNHAT(28/08/2021)
+     */
+    show(mode,id) {
+      this.employeeId = id;
+      if (mode == 0) {
+        this.employee = {};
+        this.autoNewEmployeeCode();
+      } else {
+        this.bindDataToForm();
+      }
+    },
+
+    /**-----------------------------------------------------------------
+     * Hàm sinh tự động mã nhân viên
+     * CreateBy: LQNhat (28/08/2021)
+     */
+    autoNewEmployeeCode() {
+      let self = this;
+      axios
+        .get(`https://localhost:44383/api/v1/employees/newEmployeeCode`)
+        .then((res) => {
+          let newEmployee = {};
+          newEmployee.EmployeeCode = res.data;
+          self.employee = newEmployee;
+          self.$refs.txtEmployeeCode.focus();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+
+    /**---------------------------------------------------------
+     * Hàm bind dữ liệu từ table lên form thông tin chi tiết
+     * CreateBy: LQNhat(28/08/2021)
+     */
+    bindDataToForm() {
+      var self = this;
+      // call api
+      axios
+        .get(`https://localhost:44383/api/v1/employees/${self.employeeId}`)
+        .then((res) => {
+          self.employee = res.data;
+          // format salary,date về đúng định dạng
+          self.employee.DateOfBirth = self.formatDate(res.data.DateOfBirth);
+          self.employee.IdentityDate = self.formatDate(res.data.IdentityDate);
+          console.log(res.data);
+          debugger; // eslint-disable-line
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    /**----------------------------------------------------------
+     * Hàm format ngày tháng năm trên form chỉnh sửa
+     * CreateBy:LQNhat(28/08/2021)
+     */
+    formatDate(date) {
+      if (date) {
+        return moment(String(date)).format("yyyy-MM-DD");
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- MISA.AMIS.FE/misa-amis-vue/src/view/EmployeeList.vue
+++ MISA.AMIS.FE/misa-amis-vue/src/view/EmployeeList.vue
@@ -3,7 +3,7 @@
     <div class="title-content">
       <BaseTitle :title="title" />
       <div class="btn-add-employee">
-        <button class="m-btn">
+        <button class="m-btn" @click="openModal">
           <div class="text-add">Thêm mới nhân viên</div>
         </button>
       </div>
@@ -18,232 +18,168 @@
           />
           <div class="icon-search"></div>
         </div>
+
         <div class="reload"></div>
         <div class="export"></div>
       </div>
     </div>
-      <div class="table-employee">
-        <table border="0" cellspacing="0" width="100%">
-          <thead>
-            <th><input type="checkbox" style="width: 16px; height: 18px" /></th>
-            <th>MÃ NHÂN VIÊN</th>
-            <th>TÊN NHÂN VIÊN</th>
-            <th>GIỚI TÍNH</th>
-            <th>NGÀY SINH</th>
-            <th>SỐ CMND</th>
-            <th>CHỨC DANH</th>
-            <th>TÊN ĐƠN VỊ</th>
-            <th>SỐ TÀI KHOẢN</th>
-            <th>TÊN NGÂN HÀNG</th>
-            <th>CHI NHÁNH TK NGÂN HÀNG</th>
-            <th>CHỨC NĂNG</th>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <input type="checkbox" style="width: 16px; height: 18px" />
-              </td>
-              <td>NV-0001</td>
-              <td>Lê Quý Nhật</td>
-              <td>Nam</td>
-              <td>08/11/1999</td>
-              <td>0123456</td>
-              <td>Nhân viên</td>
-              <td>Phòng đào tạo</td>
-              <td>0123456</td>
-              <td>ACB Bank</td>
-              <td>Hà Thành</td>
-              <td>Sửa</td>
-            </tr>
-            <tr>
-              <td><input type="checkbox" /></td>
-              <td>NV-0001</td>
-              <td>Lê Quý Nhật</td>
-              <td>Nam</td>
-              <td>08/11/1999</td>
-              <td>0123456</td>
-              <td>Nhân viên</td>
-              <td>Phòng đào tạo</td>
-              <td>0123456</td>
-              <td>ACB Bank</td>
-              <td>Hà Thành</td>
-              <td>Sửa</td>
-            </tr>
-            <tr>
-              <td><input type="checkbox" /></td>
-              <td>NV-0001</td>
-              <td>Lê Quý Nhật</td>
-              <td>Nam</td>
-              <td>08/11/1999</td>
-              <td>0123456</td>
-              <td>Nhân viên</td>
-              <td>Phòng đào tạo</td>
-              <td>0123456</td>
-              <td>ACB Bank</td>
-              <td>Hà Thành</td>
-              <td>Sửa</td>
-            </tr>
-            <tr>
-              <td><input type="checkbox" /></td>
-              <td>NV-0001</td>
-              <td>Lê Quý Nhật</td>
-              <td>Nam</td>
-              <td>08/11/1999</td>
-              <td>0123456</td>
-              <td>Nhân viên</td>
-              <td>Phòng đào tạo</td>
-              <td>0123456</td>
-              <td>ACB Bank</td>
-              <td>Hà Thành</td>
-              <td>Sửa</td>
-            </tr>
-            <tr>
-              <td><input type="checkbox" /></td>
-              <td>NV-0001</td>
-              <td>Lê Quý Nhật</td>
-              <td>Nam</td>
-              <td>08/11/1999</td>
-              <td>0123456</td>
-              <td>Nhân viên</td>
-              <td>Phòng đào tạo</td>
-              <td>0123456</td>
-              <td>ACB Bank</td>
-              <td>Hà Thành</td>
-              <td>Sửa</td>
-            </tr>
-            <tr>
-              <td><input type="checkbox" /></td>
-              <td>NV-0001</td>
-              <td>Lê Quý Nhật</td>
-              <td>Nam</td>
-              <td>08/11/1999</td>
-              <td>0123456</td>
-              <td>Nhân viên</td>
-              <td>Phòng đào tạo</td>
-              <td>0123456</td>
-              <td>ACB Bank</td>
-              <td>Hà Thành</td>
-              <td>Sửa</td>
-            </tr>
-            <tr>
-              <td><input type="checkbox" /></td>
-              <td>NV-0001</td>
-              <td>Lê Quý Nhật</td>
-              <td>Nam</td>
-              <td>08/11/1999</td>
-              <td>0123456</td>
-              <td>Nhân viên</td>
-              <td>Phòng đào tạo</td>
-              <td>0123456</td>
-              <td>ACB Bank</td>
-              <td>Hà Thành</td>
-              <td>Sửa</td>
-            </tr>
-            <tr>
-              <td><input type="checkbox" /></td>
-              <td>NV-0001</td>
-              <td>Lê Quý Nhật</td>
-              <td>Nam</td>
-              <td>08/11/1999</td>
-              <td>0123456</td>
-              <td>Nhân viên</td>
-              <td>Phòng đào tạo</td>
-              <td>0123456</td>
-              <td>ACB Bank</td>
-              <td>Hà Thành</td>
-              <td>Sửa</td>
-            </tr>
-            <tr>
-              <td><input type="checkbox" /></td>
-              <td>NV-0001</td>
-              <td>Lê Quý Nhật</td>
-              <td>Nam</td>
-              <td>08/11/1999</td>
-              <td>0123456</td>
-              <td>Nhân viên</td>
-              <td>Phòng đào tạo</td>
-              <td>0123456</td>
-              <td>ACB Bank</td>
-              <td>Hà Thành</td>
-              <td>Sửa</td>
-            </tr>
-            <tr>
-              <td><input type="checkbox" /></td>
-              <td>NV-0001</td>
-              <td>Lê Quý Nhật</td>
-              <td>Nam</td>
-              <td>08/11/1999</td>
-              <td>0123456</td>
-              <td>Nhân viên</td>
-              <td>Phòng đào tạo</td>
-              <td>0123456</td>
-              <td>ACB Bank</td>
-              <td>Hà Thành</td>
-              <td>Sửa</td>
-            </tr>
-            <tr>
-              <td><input type="checkbox" /></td>
-              <td>NV-0001</td>
-              <td>Lê Quý Nhật</td>
-              <td>Nam</td>
-              <td>08/11/1999</td>
-              <td>0123456</td>
-              <td>Nhân viên</td>
-              <td>Phòng đào tạo</td>
-              <td>0123456</td>
-              <td>ACB Bank</td>
-              <td>Hà Thành</td>
-              <td>Sửa</td>
-            </tr>
-            <tr>
-              <td><input type="checkbox" /></td>
-              <td>NV-0001</td>
-              <td>Lê Quý Nhật</td>
-              <td>Nam</td>
-              <td>08/11/1999</td>
-              <td>0123456</td>
-              <td>Nhân viên</td>
-              <td>Phòng đào tạo</td>
-              <td>0123456</td>
-              <td>ACB Bank</td>
-              <td>Hà Thành</td>
-              <td>Sửa</td>
-            </tr>
-          </tbody>
-        </table>
+    <div class="table-employee">
+      <table border="0" cellspacing="0" width="100%">
+        <thead>
+          <th><input type="checkbox" style="width: 16px; height: 18px" /></th>
+          <th>MÃ NHÂN VIÊN</th>
+          <th>TÊN NHÂN VIÊN</th>
+          <th>GIỚI TÍNH</th>
+          <th>NGÀY SINH</th>
+          <th>SỐ CMND</th>
+          <th>CHỨC DANH</th>
+          <th>TÊN ĐƠN VỊ</th>
+          <th>SỐ TÀI KHOẢN</th>
+          <th>TÊN NGÂN HÀNG</th>
+          <th>CHI NHÁNH TK NGÂN HÀNG</th>
+          <th>CHỨC NĂNG</th>
+        </thead>
+        <tbody>
+          <tr
+            v-for="employee in employees"
+            :key="employee.EmployeeId"
+            @dblclick="rowClick(employee.EmployeeId)"
+          >
+            <td>
+              <input type="checkbox" style="width: 16px; height: 18px" />
+            </td>
+            <td>{{ employee.EmployeeCode }}</td>
+            <td :title="employee.FullName">{{ employee.FullName }}</td>
+            <td>{{ employee.GenderName }}</td>
+            <td>{{ formatDate(employee.DateOfBirth) }}</td>
+            <td>{{ employee.IdentityNumber }}</td>
+            <td>{{ employee.PositionName }}</td>
+            <td>{{ employee.DepartmentName }}</td>
+            <td>{{ employee.AccountNumber }}</td>
+            <td>{{ employee.BankName }}</td>
+            <td>{{ employee.BranchName }}</td>
+            <td>Sửa</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="paging">
+      <div class="amount-record">Tổng số : 46 bản ghi</div>
+      <div class="page-size">
+        <Dropdown :data="dataPage" @get="getValPageSize" />
       </div>
-      <div class="paging">
-        <div class="amount-record">Tổng số : 46 bản ghi</div>
-        <div class="page-size">
-          <select id="select-size">
-            <option value="10">10 bản ghi trên 1 trang</option>
-            <option value="20">20 bản ghi trên 1 trang</option>
-            <option value="30">30 bản ghi trên 1 trang</option>
-            <option value="40">40 bản ghi trên 1 trang</option>
-          </select>
-        </div>
-        <div class="index-page">
-          <div class="index-paging btn-pre">Trước</div>
-          <div class="index-paging page-number">1</div>
-          <div class="index-paging page-number">2</div>
-          <div class="index-paging page-number">...</div>
-          <div class=" index-paging page-number">52</div>
-          <div class="index-paging btn-next">Sau</div>
-        </div>
+      <div class="index-page">
+        <div class="index-paging btn-pre">Trước</div>
+        <div class="index-paging page-number">1</div>
+        <div class="index-paging page-number">2</div>
+        <div class="index-paging page-number">3</div>
+        <div class="index-paging btn-next">Sau</div>
       </div>
     </div>
+    <EmployeeDetail
+      :isOpenModal="isOpenModal"
+      @openModal="openModal"
+      ref="modeForm"
+      :mode="modeFormDetail"
+    />
+  </div>
 </template>
 
 <script>
 import BaseTitle from "../components/base/BaseTitle.vue";
+import EmployeeDetail from "./EmployeeDetail.vue";
+import Dropdown from "../components/base/BaseDropdown.vue";
+import axios from "axios";
+import moment from "moment";
 export default {
   components: {
     BaseTitle,
+    EmployeeDetail,
+    Dropdown,
   },
   data() {
     return {
+      // data cho BaseTitle
       title: "Nhân viên",
+      // mở modal
+      isOpenModal: true,
+      // data cho dropdown paging
+      dataPage: [
+        { Text: "10 bản ghi trên 1 trang", Value: 10 },
+        { Text: "20 bản ghi trên 1 trang", Value: 20 },
+        { Text: "30 bản ghi trên 1 trang", Value: 30 },
+        { Text: "40 bản ghi trên 1 trang", Value: 40 },
+        { Text: "50 bản ghi trên 1 trang", Value: 50 },
+      ],
+      // số bản ghi trên 1 trang
+      pageSize: 0,
+      // 1 object nhân viên
+      employee: {},
+      // mảng các object nhân viên
+      employees: [],
+      // mode form detail: 0 là add, 1 là edit
+      modeFormDetail: 0,
     };
+  },
+  created() {
+    this.loadData();
+  },
+  methods: {
+    /**------------------------------------------------
+     * Bắt sự kiện mở form chi tiết
+     * CreatedBy: LQNHAT(28/08/2021)
+     */
+    openModal() {
+      this.isOpenModal = !this.isOpenModal;
+      this.modeFormDetail = 0;
+      this.$refs.modeForm.show(this.modeFormDetail);
+    },
+
+    /**---------------------------------------------------
+     * Bắt sự kiện dbclik hoặc khi ấn vào nút sửa
+     * CreatedBy: LQNHAT(28/08/2021)
+     */
+    rowClick(employeeId) {
+      this.isOpenModal = !this.isOpenModal;
+      this.modeFormDetail = 1;
+      this.$refs.modeForm.show(this.modeFormDetail,employeeId);
+      debugger; // eslint-disable-line
+    },
+
+    /**--------------------------------------------------
+     * Set value cho pageSize
+     * CreatedBy: LQNHAT(28/08/2021)
+     */
+    getValPageSize(valuePageSize) {
+      this.pageSize = valuePageSize;
+      console.log(this.pageSize);
+    },
+
+    /**-------------------------------------------------------
+     * Load data lên table
+     * CreatedBy: LQNHAT(28/08/2021)
+     */
+    loadData() {
+      var self = this;
+      axios
+        .get(`https://localhost:44383/api/v1/employees`)
+        .then((res) => {
+          self.employees = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    /**----------------------------------------------------------
+     * Hàm format ngày tháng năm trên table
+     * CreateBy:LQNhat(28/08/2021)
+     */
+    formatDate(value) {
+      if (value) {
+        return moment(String(value)).format("DD/MM/YYYY");
+      }
+    },
   },
 };
 </script>
@@ -254,4 +190,7 @@ export default {
 @import "../css/base/input.css";
 @import "../css/base/table.css";
 @import "../css/base/paging.css";
+@import "../css/employee/detail.css";
+@import "../css/base/width-input.css";
+@import "../css/base/dropdown.css";
 </style>
